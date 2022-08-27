2d3f04ebeb61ebfe18c6333b31ff6f0480c7a8e4
Pratham Bhatnagar <84272138+pratham-bhatnagar@users.noreply.github.com>
2022-08-27 21:43:39 +0530

    :construction: Work in progress

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import Nav from "./components/Nav";
+import "@rainbow-me/rainbowkit/styles.css";
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+  darkTheme,
+} from "@rainbow-me/rainbowkit";
+import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
 
-function App() {
+const { chains, provider } = configureChains(
+  [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum],
+  [alchemyProvider({ alchemyId: process.env.ALCHEMY_ID }), publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "My RainbowKit App",
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
+
+export default function App() {
   return (
-    <div className="App">
-      <Nav />
-    </div>
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider coolMode chains={chains}>
+        <Nav />
+      </RainbowKitProvider>
+    </WagmiConfig>
   );
 }
-
-export default App;
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,13 +1,12 @@
 import React from "react";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
 
 const Nav = () => {
   return (
     <nav className="w-full absolute top-0 p-2 flex justify-between">
       <h1 className="text-2xl font-black text-gradient">Web3</h1>
 
-      <button className=" text-white font-black rounded p-3 cursor-pointer bg-gradient">
-        <h1 className="text-yellow-900">Connect Wallet</h1>
-      </button>
+      <ConnectButton accountStatus="avatar" chainStatus="icon" />
     </nav>
   );
 };
