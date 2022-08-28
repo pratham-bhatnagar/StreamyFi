25f1a006d3f2dfce9b0b223117877eaab7097e3f
lakshya-dhariwal <lakshyadhariwal9@gmail.com>
2022-08-28 07:36:39 +0530

    fix table map

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,23 +1,24 @@
 import React from "react";
+import { secondsToDhms } from "../utils";
 
 function Table({ data }) {
   return (
     <>
-      <div className="grid grid-cols-5 text-yellow-500 text-xl">
-        <h1>Status</h1>
-        <h1>Stream ID</h1>
-        <h1>Withdrawn</h1>
-        <h1>Unlocked (Returned)</h1>
-        <h1>Actions</h1>
+      <div className="grid grid-cols-4 text-gradient text-xl">
+        <h1>Name</h1>
+        <h1>Time Period</h1>
+        <h1>Amount</h1>
+        <h1>Subscribed</h1>
       </div>
       <div className="">
         {data.map((row) => (
-          <ul className="text-slate-200  grid grid-cols-5 text-lg">
-            <li className="">{row.status}</li>
-            <li className="">{row.streamID}</li>{" "}
-            <li className="">{row.withdrawn}</li>
-            <li className="">{row.unlocked}</li>{" "}
-            <li className="">{row.actions}</li>
+          <ul className="text-slate-200  grid grid-cols-4 text-lg">
+            <li className="ml-1">{row.name}</li>
+            <li className="ml-1">{secondsToDhms(row.period)}</li>
+            <li className="ml-1">{row.amount}</li>
+            <li className="ml-1">
+              {row.subscribed ? "Subscribed" : "Not Subscribed"}
+            </li>
           </ul>
         ))}
       </div>
