e4871213fb6fd6db622f2393bbc2b1a7dca9e209
lakshya-dhariwal <lakshyadhariwal9@gmail.com>
2022-08-28 06:09:13 +0530

    routing

--- src/App.js
+++ src/App.js
@@ -8,6 +8,13 @@ import {
 import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
 import { alchemyProvider } from "wagmi/providers/alchemy";
 import { publicProvider } from "wagmi/providers/public";
+import {  Router, Route, Link } from "wouter";
+import Home from "./pages/Home";
+import Streams from "./pages/Streams";
+import Incoming from "./pages/Incoming";
+import Outgoing from "./pages/Outgoing";
+import AddService from "./pages/AddService";
+import Landing from "./pages/Landing";
 
 const { chains, provider } = configureChains(
   [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum],
@@ -30,6 +37,14 @@ export default function App() {
     <WagmiConfig client={wagmiClient}>
       <RainbowKitProvider coolMode chains={chains}>
         <Nav />
+        <Router>
+          <Route path="/home" component={Home} />
+          <Route path="/incoming" component={Incoming} />
+          <Route path="/outgoing" component={Outgoing} />
+          <Route path="/streams" component={Streams} />
+          <Route path="/add-service" component={AddService} />
+          <Route path="/" component={Landing} />
+        </Router>
       </RainbowKitProvider>
     </WagmiConfig>
   );
--- src/pages/AddService.jsx
+++ src/pages/AddService.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import Layout from '../layout'
+import React from "react";
+import Layout from "../layout";
 
 function AddService() {
   return (
-    <Layout>AddService</Layout>
-  )
+    <Layout>
+      <div className="p-[2px] rounded  bg-gradient w-fit">
+        <form className="bg-zinc-800 p-3">
+          <div className="">
+            <label htmlFor="">Yo</label>
+            <input type="text" />
+            <input type="text" />
+          </div>
+        </form>
+      </div>
+    </Layout>
+  );
 }
 
-export default AddService
+export default AddService;
